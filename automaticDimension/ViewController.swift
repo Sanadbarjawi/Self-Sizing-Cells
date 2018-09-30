faade91f445aabe6027806a2bb034641b6f5b305
Sanad Barjawi <sanadbarjawi@Khalids-iMac.local>
2018-09-30 09:23:27 +0300

    Initial Commit

---
+++ automaticDimension/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  automaticDimension
+//
+//  Created by Sanad Barjawi on 9/30/18.
+//  Copyright © 2018 Sanad Barjawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
